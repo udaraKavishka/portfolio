9e40cfc9babf619c9bbb7b994ef8f466f25c2a70
Udara Nalawansa <udarakavishka13@gmail.com>
2024-06-01 10:30:25 +0530

    feat: Bento grid item updation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,16 +18,16 @@ const Hero = () => {
         <div className='flex justify-center relative my-20 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 '>
-                Dynamic Data Magic for Enhanced Performance
+                An Undergraduate's Tech Adventures
                 </h2>
 
                 <TextGenerateEffect
                     className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                    words='Transforming Data into Seamless, Predictive Insights '
+                    words='Unleashing Potential in Technology and Innovation '
                 />
 
                 <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
-                    Hi, I&apos;m Udara, a Data Analyst based in Sri Lanka
+                    Hi, I&apos;m Udara, an Undergraduate  based in Colombo
                 </p>
 
                 <a href="#about">
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from './GridGlobe'
 
 export const BentoGrid = ({
   className,
@@ -11,7 +12,7 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -45,7 +46,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4 border-white/[0.5] ",
         className
       )}
       style={{
@@ -55,14 +56,13 @@ export const BentoGridItem = ({
           "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
       }}
     >
-
-      <div className={`${id === 6} && 'flex justify-center h-full`}>
+      <div className={`${id === 6 && 'flex justify-center'} h-full`}>
         <div className="w-full h-full absolute">
           {img && (
             <img 
                 src={img}
                 alt={img}
-                className={cn(imgClassName,'object-cover,object-center')}
+                className={cn(imgClassName,'object-cover object-center')}
             />
           )}
 
@@ -72,18 +72,18 @@ export const BentoGridItem = ({
               <img 
                   src={spareImg}
                   alt={spareImg}
-                  className={'object-cover,object-center w-full h-full'}
+                  className={'object-cover object-center w-full h-full'}
               />
             )}    
           </div>
             {id ===6 &&(
               <BackgroundGradientAnimation>
-                <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+                <div className="absolute z-50 inset-0 flex items-center justify-center text-white font-bold "/>
               </BackgroundGradientAnimation>
             )}
 
             <div className={cn(
-              titleClassName,'group-hover/ bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
+              titleClassName,'group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10'
             )}>
               <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
                 {description}
@@ -91,14 +91,31 @@ export const BentoGridItem = ({
               <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
                   {title}
               </div>
-            </div>
-      </div>
+            
+            {id ===2 && <GlobeDemo />}
 
-      
-      <div className="group-hover/bento:translate-x-2 transition duration-200">
-        
-        
+            {id ===3 &&(
+              <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+                <div className="flex flex-col gap3 lg:gap-8">
+                  {['React.js', 'Next.js', 'Typescript'].map((item)=>(
+                    <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                      {item}
+                    </span>
+                  ))}
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                </div>
+                <div className="flex flex-col gap3 lg:gap-8">
+                  <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]"/>
+                  {['Python', 'C++', 'Java'].map((item)=>(
+                    <span key={item} className="py-2 lg:py4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                      {item}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            )}
       </div>
+    </div>
     </div>
   );
 };
